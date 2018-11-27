151f9ddd48e58c88f339dbec64f944c32a119ba4
julian-gomez <jd.calderon@uniandes.edu.co>
2018-11-27 16:15:58 -0500

    Plot masa

---
+++ graficaMasas.py
@@ -0,0 +1,8 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+datos = np.genfromtxt("masas.txt", delimiter = " ", usecols = (0,1))
+
+plt.figure()
+plt.plot(datos[:,0],datos[:,1])
+plt.show()
